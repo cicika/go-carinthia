200d2fe4eed2892e8043a4f84fd9caa1a8695097
Tijana Dojcinovic <tijana@toptal.com>
2016-06-25 01:37:54 +0200

    Add create tables statement

---
+++ sql/create_tables.sql
@@ -0,0 +1,53 @@
+CREATE TABLE IF NOT EXISTS users (
+  id serial PRIMARY KEY,
+  email text UNIQUE,
+  name text NOT NULL,
+  auth_token text,
+  type text NOT NULL,
+  beacon_identifier text
+);
+
+CREATE TABLE IF NOT EXISTS beacons (
+  id serial PRIMARY KEY,
+  identifier text UNIQUE,
+  user_id int REFERENCES users,
+  transport_type text NOT_NULL,
+  provider_id,
+  is_stationary boolean
+);
+
+CREATE TABLE IF NOT EXISTS providers (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  transport_type text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS trips (
+  id serial PRIMARY KEY,
+  user_id int REFERENCES users,
+  started_at bigint,
+  ended_at bigint,
+  total_price decimal
+);
+
+CREATE TABLE IF NOT EXISTS trip_segments (
+  id serial PRIMARY KEY,
+  user_id int REFERENCES users,
+  beacon_identifier text,
+  trip_id int REFERENCES trips,
+  check_in_type int,
+  transport_station_id int,
+  latitude numeric,
+  longitude numeric,
+  passed_at bigint
+);
+
+CREATE TABLE IF NOT EXISTS transport_stations (
+  id serial PRIMARY KEY,
+  name text,
+  providers_identifier int,
+  latitude numeric,
+  longitude numeric,
+  type text,
+  zone_id int
+);
